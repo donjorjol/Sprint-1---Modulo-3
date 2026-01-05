2edf59fa98e4eb361591902c4f57707d07d1fe43
Jorge Alberto Muñoz Eade <jorge.alberto82@gmail.com>
2026-01-05 15:47:29 -0300

    Subiendo Trabajo

---
+++ assets/js/app.js
@@ -0,0 +1,119 @@
+// Busqueda de receta por ingrediente
+const buscarRecetas = async (ingrediente) => {
+  if (!ingrediente) return null;
+  const url = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingrediente}`;
+
+  try {
+    const respuesta = await fetch(url);
+    const datos = await respuesta.json();
+    // console.log("Recetas encontradas:", datos.meals);
+    return datos.meals;
+  } catch (error) {
+    console.error("Error al buscar la receta:", error);
+  }
+};
+
+// Detalle de la receta por ID
+const detalleReceta = async (id) => {
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+  try {
+    const respuesta = await fetch(url);
+    const datos = await respuesta.json();
+    // console.log(datos);
+    return datos.meals[0];
+  } catch (error) {
+    console.error("Error al buscar la receta:", error);
+  }
+};
+
+const renderizarBusqueda = async () => {
+  const ingrediente = input.value.trim();
+  const errorMessage = document.getElementById("error-message");
+  const contenedor = document.getElementById("resultado");
+
+  // Validacion input
+  if (ingrediente === "") {
+    contenedor.innerHTML = "";
+    errorMessage.classList.remove("d-none");
+    input.focus();
+
+    return;
+  }
+
+  errorMessage.classList.add("d-none");
+
+  contenedor.innerHTML = `
+  <div class="w-100 d-flex justify-content-center">
+    <div class="spinner-border text-success" role="status">
+      <span class="visually-hidden">Buscando</span>
+    </div>
+  </div>`;
+
+  const recetas = await buscarRecetas(ingrediente);
+
+  if (!recetas) {
+    contenedor.innerHTML = `
+     <div class="col-12 d-flex justify-content-center">
+      <div class="alert alert-warning text-center shadow-sm w-100" role="alert" style="max-width: 500px;">
+        <h5 class="mb-2">😕 No se encontraron recetas.</h5>
+        <p class="mb-0">Intenta con otro ingrediente.</p>
+      </div>
+    </div>`;
+    return;
+  }
+
+  contenedor.innerHTML = recetas
+    .map(
+      ({ strMealThumb, strMeal, idMeal }) => `
+        <div class="col-12 col-md-6 col-lg-4">
+         <div class="recipe-card card h-100 shadow-sm border-0">
+            <img
+              src="${strMealThumb}"
+              class="card-img-top"
+              alt="${strMeal}"
+            />
+            <div class="card-body">
+              <h5 class="card-title fw-semibold">${strMeal}</h5>
+              <a href="#" class="btn btn-outline-success rounded-pill ver-receta" data-id=${idMeal}
+                >Ver receta</a
+              >
+            </div>
+          </div>
+        </div>
+      `
+    )
+    .join("");
+};
+
+const input = document.getElementById("input-ingrediente");
+input.addEventListener("keydown", async (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    renderizarBusqueda();
+  }
+});
+
+const searchBtn = document.getElementById("btn-buscar");
+searchBtn.addEventListener("click", async (e) => {
+  e.preventDefault();
+  renderizarBusqueda();
+});
+
+document.addEventListener("click", async (e) => {
+  if (!e.target.classList.contains("ver-receta")) return;
+  e.preventDefault();
+
+  const id = e.target.dataset.id;
+
+  const { strMeal, strMealThumb, strInstructions } = await detalleReceta(id);
+
+  // Pasarlos al modal
+  document.getElementById("modalTitulo").textContent = strMeal;
+  document.getElementById("modalImagen").src = strMealThumb;
+  document.getElementById("modalImagen").alt = strMeal;
+  document.getElementById("modalDetalle").textContent = strInstructions;
+
+  // Mostrar modal
+  const modal = new bootstrap.Modal(document.getElementById("modalReceta"));
+  modal.show();
+});
